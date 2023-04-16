0ab29f4f982501fdb7ec135547c7fcae25e4aba9
yw-fang <fyuewen@gmail.com>
2023-04-16 21:20:31 +0200

    script update,resolve the em dash, en dash issues in names using the to_ascii function

--- _pages/includes/doi2github.py
+++ _pages/includes/doi2github.py
@@ -1,26 +1,52 @@
 import re
 
+import unicodedata
+
+def to_ascii(in_string):
+    # Replace Unicode dashes with ASCII hyphen-minus
+    out_string = in_string.replace('\u2013', '-').replace('\u2014', '-')
+    return(out_string)
+
+# my_string = "example-string—containing–dashes"
+# print(my_string)
+# print(to_ascii(in_string=my_string))
+
+
+
 # Open the input file
 with open('1.txt', 'r') as f:
     content = f.read()
 
 # Get the author list
 author_match = re.search(r'author = {(.+)}', content)
-authors = author_match.group(1).split(' and ')
+authors_string = author_match.group(1)
+# print(authors_string)
+# Check for spaces at the beginning of the string
+while authors_string.startswith(" "):
+    authors_string = authors_string[1:]
+# Check for spaces at the end of the string
+while authors_string.endswith(" "):
+    authors_string = authors_string[:-1]
+#    print(authors_string)
+print(authors_string)
+to_ascii(authors_string)
+print(authors_string)
+authors = authors_string.split(' and ')
+print(authors)
+
 
 # Check if Yue-Wen Fang is in the author list
 yuewen_fang = False
 for i, author in enumerate(authors):
-    if 'Yue-Wen Fang' in author:
-        yuewen_fang = True
-        authors[i] = '<b>Yue-Wen Fang</b>'
-    elif 'Fang, Yue-Wen' in author:
+    if ('Yue'+'-'+'Wen') in author:
         yuewen_fang = True
         last_name, first_name = author.split(', ')
         authors[i] = f'<b>{first_name} {last_name}</b>'
     else:
         last_name, first_name = author.split()
+        print(last_name, first_name)
         authors[i] = f'{first_name} {last_name}'
+print(yuewen_fang)
 
 # Get the remaining fields
 title_match = re.search(r'title = {(.+)}', content)
@@ -33,7 +59,23 @@ doi_match = re.search(r'doi = {(.+)}', content)
 url_match = re.search(r'url = {(.+)}', content)
 
 # Generate the HTML
-html = f'<tr>\n  <td style="vertical-align: top; padding-right: 20px;">\n    <p>40. {"; ".join(authors)}, <a href="{url_match.group(1)}"><i>{title_match.group(1)}</i></a>, {journal_match.group(1)}, {year_match.group(1)}, {volume_match.group(1)}({issue_match.group(1)}): {pages_match.group(1)}.</p>\n  </td>\n  <td style="vertical-align: top;">\n    <a href="{url_match.group(1)}"><img src="./journal-cover/cover.png" alt="cover" width="500"></a>\n  </td>\n</tr>'
+html = '<tr>\n  <td style="vertical-align: top; padding-right: 20px;">\n    <p>40. {}, <a href="{}"><i>{}</i></a>, {}, {}, '.format("; ".join(authors), url_match.group(1), title_match.group(1), journal_match.group(1), year_match.group(1))
+
+if volume_match:
+    html += '{}'.format(volume_match.group(1))
+    if issue_match:
+        html += '({}): '.format(issue_match.group(1))
+    else:
+        html += ': '
+else:
+    html += ' : '
+    
+if pages_match:
+    html += '{}.'.format(pages_match.group(1))
+else:
+    html += '.'
+
+html += '</p>\n  </td>\n  <td style="vertical-align: top;">\n    <a href="{}"><img src="./journal-cover/cover.png" alt="cover" width="500"></a>\n  </td>\n</tr>'.format(url_match.group(1))
 
 # Print the HTML
 print(html)
